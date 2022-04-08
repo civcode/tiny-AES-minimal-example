3b750325e6f3fed0f19478a791ca4982124c0ee2
Ivancsits Christian <christian.ivancsits@wackerneuson.com>
2022-04-08 11:34:12 +0200

    remove commented out lines

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@ using std::endl;
 
 void PrintData(uint8_t data[], uint8_t len) {
     for (int i=0; i<len; i++) {
-        //printf("%x", static_cast<char>(data[i]));
         printf("%02x", data[i]);
     }
     printf("\n");
