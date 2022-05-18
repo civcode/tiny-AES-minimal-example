40247fbc29f4dbb07e14940ed72276bdf5257847
Ivancsits Christian <christian.ivancsits@wackerneuson.com>
2022-05-18 18:26:30 +0200

    improved naming and text output

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ using std::cout;
 using std::endl;
 
 void PrintData(uint8_t data[], uint8_t len) {
+    printf("0x");
     for (int i=0; i<len; i++) {
         printf("%02x", data[i]);
     }
@@ -18,46 +19,47 @@ void PrintData(uint8_t data[], uint8_t len) {
 
 int main() {
 
-    uint8_t key[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 
-                       0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
+    uint8_t aes_key[16] = {0xf0, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 
+                           0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
 
     const char *seed_phrase = "This is my seed!";
     uint8_t seed_phrase_size = strlen(seed_phrase);
     
-    uint8_t seed[16] = {0, 16};
+    uint8_t uds_seed[16] = {0, 16};
 
-    for (int i=0; i<sizeof(seed); i++) {
+    for (int i=0; i<sizeof(uds_seed); i++) {
         if (i<seed_phrase_size) {
-            seed[i] = *(seed_phrase+i);
+            uds_seed[i] = *(seed_phrase+i);
         }
 
     }    
 
     /* print data */
-    cout << "AES key:" << endl;
-    PrintData(key, sizeof(key));
+    cout << "AES key (128 bit):" << endl;
+    PrintData(aes_key, sizeof(aes_key));
     cout << endl;
 
     cout << "seed:" << endl;
-    cout << "seed_phrase = " << seed_phrase << endl;
-    cout << "seed_size = " << static_cast<int>(seed_phrase_size) << endl;
-    PrintData(seed, sizeof(seed));
+    cout << "seed_phrase = \"" << seed_phrase << "\"" << endl;
+    cout << "seed_size = " << static_cast<int>(seed_phrase_size) << " bytes" << endl;
+    cout << "uds_seed:" << endl;
+    PrintData(uds_seed, sizeof(uds_seed));
     cout << endl;
 
     /* encrypt data */ 
     struct AES_ctx ctx;
-    AES_init_ctx(&ctx, key);
-    AES_ECB_encrypt(&ctx, seed); 
+    AES_init_ctx(&ctx, aes_key);
+    AES_ECB_encrypt(&ctx, uds_seed); 
 
-    cout << "encrypted data (=UDS KEY):" << endl;
-    PrintData(seed, sizeof(seed));
+    cout << "encrypted uds_seed (= UDS Key):" << endl;
+    PrintData(uds_seed, sizeof(uds_seed));
     cout << endl;
 
     /* decrypt data */
-    AES_ECB_decrypt(&ctx, seed);
+    AES_ECB_decrypt(&ctx, uds_seed);
 
-    cout << "decrypted data:" << endl;
-    PrintData(seed, sizeof(seed));
+    cout << "decrypted UDS key (must match uds_seed):" << endl;
+    PrintData(uds_seed, sizeof(uds_seed));
     cout << endl;
 
     return 0;
